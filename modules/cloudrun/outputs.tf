85eaf53ef8bfad3d62f681b8a492fab553e4d3fd
piyushkg007 <piyushgunde252@gmail.com>
2024-04-05 07:09:05 +0000

    ff

---
+++ modules/cloudrun/outputs.tf
@@ -0,0 +1,3 @@
+output "cloud_run_frontendref" {
+  value = google_cloud_run_v2_service.default.name
+}
---
+++ providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "google" {
+  region                      = var.region
+  zone                        = var.zone
+  impersonate_service_account = var.service_account_email
+}
